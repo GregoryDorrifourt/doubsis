4b7e90c3eeb4722820aa1d1c7329e9e89e9ad6b1
Greg <g.dorrifourt@clair-et-net.com>
2016-03-02 18:41:01 +0100

    Commit Initial Compilation OK

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name doubsisApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the doubsisApp
+ */
+angular.module('doubsisApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
