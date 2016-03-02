b6a862094f88f6bb8125dbbf79f8b584ee0e2a01
Greg <g.dorrifourt@clair-et-net.com>
2016-03-02 17:11:45 +0100

    Commit initial

---
+++ app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name doubsisApp
+ * @description
+ * # doubsisApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('doubsisApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name doubsisApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the doubsisApp
+ */
+angular.module('doubsisApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
