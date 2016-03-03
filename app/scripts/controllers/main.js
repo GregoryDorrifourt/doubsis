af6cdc3ad5dc6f3af6791fedfd47bc4bba8f21a0
Greg <g.dorrifourt@clair-et-net.com>
2016-03-03 12:23:13 +0100

    Preprod

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,48 @@
  */
 angular.module('doubsisApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    var that = this;
+    that.nbJoueurs = 44;
+    that.checkMultiple = function(){
+      return that.nbJoueurs % 4 === 0 && that.nbJoueurs>0;
+    };
+
+    that.generer = function(){
+      if(that.checkMultiple()){
+          that.nbTables = that.nbJoueurs/4;
+          that.tableau = [];
+          for(var i=0;i<that.nbJoueurs;i++){
+            that.tableau.push(i+1);
+          }
+          that.tableau = shuffleArray(that.tableau);
+
+          that.tables = [];
+          for(var j=0;j<that.tableau.length;j++){
+            var ct = Math.floor(j/4);
+            if(j%4===0){
+              that.tables[ct] = [];
+            }
+            that.tables[ct].push(that.tableau[j]);
+          }
+
+      }
+    };
+
+    var shuffleArray = function(array) {
+        var m = array.length, t, i;
+
+        // While there remain elements to shuffle
+        while (m) {
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * m--);
+
+      // And swap it with the current element.
+      t = array[m];
+      array[m] = array[i];
+      array[i] = t;
+    }
+
+    return array;
+    };
   });
+  
